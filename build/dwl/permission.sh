8884b7a719246dae84f9b888beea69cbf2e07b2e
davask <contact@davaskweblimited.com>
2017-07-11 18:17:42 +0200

    Automatic update 8.8

--- build/dwl/permission.sh
+++ build/dwl/permission.sh
@@ -5,7 +5,7 @@ if [ ! -d /dwl/home/host ]; then
 fi
 if [ -d /dwl/home/host/files ]; then
     sudo rm -rdf /home/${DWL_USER_NAME}/files;
-    sudo ln -sf /dwl/home/host/files /home/${DWL_USER_NAME}/files;
+    sudo cp -rdf /dwl/home/host/files /home/${DWL_USER_NAME};
 fi
 
 sudo chown -R ${DWLC_USER_NAME}:${DWLC_USER_NAME} /home/${DWLC_USER_NAME}/;
