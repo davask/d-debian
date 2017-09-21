ea4d0257ad0fead493c5dfca1b570000f54519b8
davask <contact@davaskweblimited.com>
2017-09-21 12:18:17 +0200

    Automatic update 8.8

--- build/dwl/init.sh
+++ build/dwl/init.sh
@@ -14,4 +14,11 @@ echo ">> Permission assigned";
 
 . ${dwlDir}/custom.sh
 
+# . ${dwlDir}/senmail.sh
+# sendmail is only available from davask/d-php*
+if [ "0" == "1" ]; then
+  service sendmail start;
+  echo ">> Sendmail initialized";
+fi
+
 tail -f /dev/null;
